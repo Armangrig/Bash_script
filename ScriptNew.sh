f475de4de172ee4586b2b5c546f90caf3256630a
Armangrig <118439516+Armangrig@users.noreply.github.com>
2023-04-23 01:28:18 +0400

    Create ScriptNew.sh

---
+++ ScriptNew.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function get_dir_file(){
+        if [[ ! -d $1 ]]; then
+                echo "No such directory"
+                exit 1
+        fi
+}
+function dir_ls(){
+        if [[ ! -f $2 ]]; then
+                echo "No such file"
+                touch $2
+        fi
+
+        ls $1 > $2
+}
+
+get_dir_file $1
+dir_ls $1 $2
